115111652b6db0207e471cc3edcb182b3074276f
DayvonAllen <dayvonallen93@outlook.com>
2021-07-01 08:18:50 -0500

    code cleanup

--- middleware/IsLoggedIn.go
+++ middleware/IsLoggedIn.go
@@ -13,13 +13,13 @@ func IsLoggedIn(c *fiber.Ctx) error {
 	_, loggedIn, err := auth.IsLoggedIn(token)
 
 	if err != nil || loggedIn == false {
-		return c.Status(401).JSON(fiber.Map{"status": "error", "message": "error...", "data": fmt.Sprintf("%v", err)})
+		return c.Status(401).JSON(fiber.Map{"status": "error", "message": "error...", "data": fmt.Sprintf("Unauthorized user")})
 	}
 
 	err = c.Next()
 
 	if err != nil {
-		return c.Status(401).JSON(fiber.Map{"status": "error", "message": "error...", "data": fmt.Sprintf("%v", err)})
+		return c.Status(401).JSON(fiber.Map{"status": "error", "message": "error...", "data": fmt.Sprintf("Unauthorized user")})
 	}
 
 	return nil
